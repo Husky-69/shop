4cd6306b573066096cb93bf3eb276117ce248a1d
shah <shallomgithui59@gmail.com>
2024-09-25 18:50:04 +0300

    remember

---
+++ shop.py
@@ -0,0 +1,10 @@
+products = ["nescafe","apple", "blueband", "jam" ,"juice", "sugar"]
+
+
+def display_items(dis):
+    print("display items")
+    for product in dis:
+        print(f"{product}")
+
+#function_name(arguments)
+display_items(products)
